97ef6aeeb90d8f27a112bdf666b31c969831e00c
Lisa Waddell <90416736+techteacher101nj@users.noreply.github.com>
2026-01-28 18:55:27 +0000

    wrote bmi program

--- main.cpp
+++ main.cpp
@@ -3,6 +3,17 @@ using namespace std;
 
 int main() {
  
+    int height, weight;
+    height = 61;
+    weight = 130;
+    
+    double kg, meters, bmi;
 
+    kg = weight*0.453592;
+    meters = height * 0.0254;
+
+    bmi = kg/(meters*meters);
+
+    cout << "The BMI for this person is: " << bmi << endl;
 
 }
